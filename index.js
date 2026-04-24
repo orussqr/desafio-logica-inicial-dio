065ad0c2ca17e39586d313b9f651f23ddad23819
orussqr <vitorssqr@gmail.com>
2026-04-24 11:38:57 -0300

    Entrega atividade lógica de progamação do Felipão

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("1 milão")
+//Atividade DIO professor Felipão
+
+//Desafio Classificador de nível de Herói
+
+let xp = 7001;
+let nick = "Felipão";
+
+if (xp <= 1000){
+    console.log("O Herói de nome " + nick + " está no nível Ferro");
+  }  
+  else if(xp <= 2000){
+    console.log("O Herói de nome " + nick + " está no nível Bronze");
+  }  
+  else if(xp <= 5000) {
+    console.log("O Herói de nome " + nick + " está no nível Prata");
+  }  
+  else if(xp <= 7000){
+    console.log("O Herói de nome " + nick + " está no nível Ouro");
+  } 
+   else if(xp <= 8000){
+    console.log("O Herói de nome " + nick + " está no nível Platina");
+  } 
+  else if(xp <= 9000){
+    console.log("O Herói de nome " + nick + " está no nível Ascendente");
+  }  
+  else if(xp <= 10000){
+    console.log("O Herói de nome " + nick + " está no nível Imortal");
+  } 
+
+    else{
+    console.log("O Herói de nome " + nick + " está no nível Radiante");
+    }
+
---
+++ readme.txt
@@ -0,0 +1,20 @@
+O script classifica o nível de um herói com base na quantidade de XP (experiência) informada.
+
+A lógica utiliza estruturas condicionais (if / else if / else) 
+para determinar em qual faixa de nível o herói se encontra.
+______________________________________________________________________________________________
+
+Foi definido duas variáveis:
+
+nick: nome do herói
+xp: quantidade de experiência do herói
+
+______________________________________________________________________________________________
+
+Como executar:
+
+Instale o Node.js
+Salve o arquivo como index.js
+Execute no terminal: node index.js
+
+______________________________________________________________________________________________
